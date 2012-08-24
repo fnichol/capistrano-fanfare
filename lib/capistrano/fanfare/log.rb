f3f7a654cb7d92bff26fe7be204604b725c82df1
Fletcher Nichol <fnichol@nichol.ca>
2012-08-24 14:33:35 -0600

    Add cap log:view task.

--- lib/capistrano/fanfare/log.rb
+++ lib/capistrano/fanfare/log.rb
@@ -1,9 +1,29 @@
 require 'capistrano'
+require 'tempfile'
 
 module Capistrano::Fanfare::Log
   def self.load_into(configuration)
     configuration.load do
       set :tail_cmd, "tail"
+
+      def resolve_logfile
+        rack_env = fetch(:rails_env, nil) || fetch(:rack_env, nil) || "production"
+
+        File.join(current_path, "log", "#{rack_env}.log")
+      end
+
+      def resolve_editor
+        ENV['editor'] || ENV['EDITOR'] || ENV['VISUAL'] || "vi"
+      end
+
+      def log_exec(cmd)
+        exec cmd
+      end
+
+      # =========================================================================
+      # These are the tasks that are available to help with deploying web apps.
+      # You can have cap give you a summary of them with `cap -T'.
+      # =========================================================================
 
       namespace :log do
         desc <<-DESC
@@ -14,9 +34,53 @@ module Capistrano::Fanfare::Log
               set :tail_cmd,  "tail"
         DESC
         task :tail, :role => :app, :except => { :no_release => true } do
-          rack_env = fetch(:rails_env, nil) || fetch(:rack_env, nil) || "production"
+          stream("#{tail_cmd} -f #{resolve_logfile}")
+        end
 
-          stream("#{tail_cmd} -f #{current_path}/log/#{rack_env}.log")
+        desc <<-DESC
+          View log files in local editor. You can set the rails environment \
+          and tail command by setting variables. The defaults are:
+
+              set :rails_env, "production"
+              set :tail,      "tail"
+
+          To override the default number of lines (500), you can pass in \
+          the `n' environment variable like so:
+
+              $ cap log:view n=900
+
+          To override your EDITOR/VISUAL environment settings for your visual \
+          editor, you can pass in the `editor' environment variable like so:
+
+              $ cap log:view editor=nano
+
+          Otherwise this task will try to resolve your editor in the \
+          following order:
+
+              1) use the `editor' environment override variable value
+              2) use the `EDITOR' shell environment variable value
+              3) use the `VISUAL' shell environment variable value
+              4) use `vi' which should be in almost any PATH
+        DESC
+        task :view, :role => :app, :except => { :no_release => true } do
+          line_nums = ENV["n"] || 500
+          cmd       = "#{tail_cmd} -n #{line_nums} #{resolve_logfile}"
+          logs      = Hash.new { |h,k| h[k] = '' }
+
+          run(cmd) do |channel, stream, data|
+            logs[channel[:host]] << data
+            break if stream == :err
+          end
+
+          tmpfile = Tempfile.open('w')
+          logs.each do |host, log|
+            tmpfile.write("--- #{host} ---\n\n")
+            tmpfile.write(log + "\n")
+          end
+          tmpfile.flush
+          tmpfile.close
+
+          log_exec "#{resolve_editor} #{tmpfile.path}"
         end
       end
     end
--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/capistrano'
 require 'capistrano/fanfare'
 require 'capistrano/fanfare/log'
+require 'mocha'
 
 describe Capistrano::Fanfare::Log do
   before do
@@ -41,6 +42,77 @@ describe Capistrano::Fanfare::Log do
         @config.find_and_execute_task("log:tail")
 
         @config.must_have_streamed "/usr/bin/tail -f /my/path/log/production.log"
+      end
+    end
+
+    describe "task :view" do
+      before do
+        @config.stubs(:log_exec).returns(true)
+        @tempfile = Tempfile.open('w')
+        Tempfile.stubs(:open).with('w').returns(@tempfile)
+      end
+
+      describe "resolving log file" do
+        it "runs a tail on a log file based on :rails_env variable" do
+          @config.set :rails_env, "uat"
+          @config.find_and_execute_task("log:view")
+
+          @config.must_have_run "/usr/bin/tail -n 500 /my/path/log/uat.log"
+        end
+
+        it "runs a tail on a log file based on :rack_env variable" do
+          @config.set :rack_env, "staging"
+          @config.find_and_execute_task("log:view")
+
+          @config.must_have_run "/usr/bin/tail -n 500 /my/path/log/staging.log"
+        end
+
+        it "runs a tail of the production log file if :rails_env and :rack_env does not exist" do
+          @config.find_and_execute_task("log:view")
+
+          @config.must_have_run "/usr/bin/tail -n 500 /my/path/log/production.log"
+        end
+      end
+
+      it "runs a tail on a log file with a specified line count" do
+        ENV['_SPEC_n'] = ENV['n']
+        ENV['n'] = '987'
+        @config.find_and_execute_task("log:view")
+
+        @config.must_have_run "/usr/bin/tail -n 987 /my/path/log/production.log"
+        ENV['n'] = ENV.delete('_SPEC_n')
+      end
+
+      describe "resolving an editor" do
+        ENV_EDITORS = %w{VISUAL EDITOR editor}
+
+        before do
+          ENV_EDITORS.each do |e|
+            ENV["_SPEC_#{e}"] = ENV[e]
+            ENV.delete(e)
+          end
+        end
+
+        after do
+          ENV_EDITORS.each do |e|
+            ENV[e] = ENV.delete("_SPEC_#{e}")
+          end
+        end
+
+        it "opens the logfile in vi by default" do
+          @config.expects(:log_exec).with("vi #{@tempfile.path}")
+
+          @config.find_and_execute_task("log:view")
+        end
+
+        ENV_EDITORS.each do |v|
+          it "opens the logfile in the editor in ENV['#{v}']" do
+            ENV[v] = "/this/editor/#{v}"
+            @config.expects(:log_exec).with("#{ENV[v]} #{@tempfile.path}")
+
+            @config.find_and_execute_task("log:view")
+          end
+        end
       end
     end
   end
